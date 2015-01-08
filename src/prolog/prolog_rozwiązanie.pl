ac43fbf7e30c72986c454f607b00121b17a4b092
lufcik16 <lustykt@student.mini.pw.edu.pl>
2015-01-08 21:25:07 +0100

    Commit  - wszystko zrobione. Baza danych postawiona, zrobione ładne menu. Prowadząca będzie zachwycona :)

--- src/prolog/prolog_rozwiązanie.pl
+++ src/prolog/prolog_rozwiązanie.pl
@@ -2,11 +2,45 @@
 :- prompt(_, '').
 :- use_module(library(readutil)).
 :- use_module(library(lists)).
+:- dynamic macierz_db/2.
+
+macierz(0, [1, 2, 3, 4, 5, 6]).
+macierz(1, [[1,2,3],[4,5,6],[7,8,9]]).
+macierz(2, [[10,20,30],[40,50,60],[70,80,90]]).
+macierz(3, [[[1, 2],[3, 4]],[[5, 6],[7, 8]]]).
+macierz(4, [[[10, 20],[30, 40]],[[50, 60],[70, 80]]]).
+macierz(5, [[1,0,0],[0,1,0],[0,0,1]]).
+macierz(6, [[[1,10],[2,20],[3,30]],[[4,40],[5,50],[6,60]],[[7,70],[8,80],[9,90]]]).
+macierz(7, [[[1,2],[3,4]],[[5,6],[7,8]],[[9,10],[11,12]]]).
+
 
 main:-
-	process,
+	assert_database,
+	main_menu(8),
+	clear_database,
 	halt.
 
+assert_database:-
+	macierz(IdMacierzy, Macierz),
+	assertz(macierz_db(IdMacierzy, Macierz)),
+	fail.
+assert_database:- !.
+
+clear_database:-
+	retract(macierz_db(_,_)),
+	fail.
+clear_database :- !.
+
+wyswietl_macierze(X,X):-
+	write('\n'),
+	write('To ju¿ wszystkie macierze'),!.
+wyswietl_macierze(IdMacierzy,MaxNrMatrix):-
+	macierz_db(IdMacierzy,Macierz),
+	write('Id='),write(IdMacierzy),
+	write('; Macierz='),write(Macierz),
+	write('\n'),
+	NextId is IdMacierzy+1,
+	wyswietl_macierze(NextId,MaxNrMatrix).
 
 insert(Val,[H|List],Pos,[H|Res]):-
 	Pos > 1,
@@ -104,128 +138,149 @@ swap_advanced(A,From,To,Result):-
 	nth0(FromX2,Res5,_,Res7),
 	insert(Res6,Res7,FromX,Result).
 
-
-
-
-
-
-process:-
-	M0 = [1, 2, 3, 4, 5, 6],
-	write('M0 = '),
-	write(M0),
+dodaj_macierz(NewID):-
 	write('\n'),
-	M1 = [[1,2,3],[4,5,6],[7,8,9]],
-	write('M1 = '),
-	write(M1),
+	write('Podaj Macierz: '),
+	read(Macierz),
+	assertz(macierz_db(NewID,Macierz)),
 	write('\n'),
-	M2 = [[10,20,30],[40,50,60],[70,80,90]],
-	write('M2 = '),
-	write(M2),
-	write('\n'),
-	M3 = [[[1, 2],[3, 4]],[[5, 6],[7, 8]]],
-	write('M3 = '),
-	write(M3),
-	write('\n'),
-	M4 = [[[10, 20],[30, 40]],[[50, 60],[70, 80]]],
-	write('M4 = '),
-	write(M4),
-	write('\n'),
-	M5 = [[1,0,0],[0,1,0],[0,0,1]],
-	write('M5 = '),
-	write(M5),
-	write('\n'),
-	add(M1, M2, M6),
-	write('M1 + M2 = '),
-	write(M6),
-	write('\n'),
-	add(M3, M4, M7),
-	write('M3 + M4 = '),
-	write(M7),
-	write('\n'),
-	scale(M1, 3, M8),
-	write('M1 * 3 = '),
-	write(M8),
-	write('\n'),
-	scale(M3, 2, M9),
-	write('M3 * 2 = '),
-	write(M9),
-	write('\n'),
-	multiply(M1, M2, M10),
-	write('M1 * M2 = '),
-	write(M10),
-	write('\n'),
-	multiply(M1, M5, M11),
-	write('M1 * M5 = '),
-	write(M11),
-	write('\n'),
-	transpose(M1, M12),
-	write('transpose(M1) = '),
-	write(M12),
-		write('\n'),
-	transpose(M2, M13),
-	write('transpose(M2) = '),
-	write(M13),
-			write('\n'),
-	transpose(M4, M14),
-	write('transpose(M4) = '),
-	write(M14),
-		write('\n'),
-		write('swap 2nd and 5th element [1, 2, 3, 4, 5, 6] = '),
-		swap([1, 2, 3, 4, 5, 6],2,5,Result3),
-	write(Result3),
-		write('\n'),
-		write('swap 1nd and 3rd element [[1,2,3],[4,5,6],[7,8,9]] = '),
-		swap([[1,2,3],[4,5,6],[7,8,9]],1,3,Result4),
-	write(Result4),
-			write('\n'),
-		write('swap 1nd and 2nd element [[[1,10],[2,20],[3,30]],[[4,40],[5,50],[6,60]],[[7,70],[8,80],[9,90]]] = '),
-		swap([[[1,10],[2,20],[3,30]],[[4,40],[5,50],[6,60]],[[7,70],[8,80],[9,90]]],1,2,Result5),
-	write(Result5),
-
-	write('\n'),
-	write('\n'),
-	write('SWAP ADVANCED'),
-	write('\n'),
-	swap_advanced([[[1,2],[3,4]],[[5,6],[7,8]],[[9,10],[11,12]]],[1,2],[2,1],Result),
-	write('\n'),
-	write('Zamiana w macierzy:'),
-	write('\n'),
-	write('[[ 1, 2],[ 3, 4]]'),
-	write('\n'),
-	write('[[ 5, 6],[ 7, 8]]'),
-	write('\n'),
-	write('[[ 9,10],[11,12]]'),
-	write('\n'),
-	write('elementÃ³w [3,4] z [5,6]'),
-	write('\n'),
-	write(Result),
-	write('\n'),
-	write('\n'),
-	swap_advanced([[1,2,3],[4,5,6],[7,8,9]],[2,2],[3,1],Result2),
-	write('\n'),
-	write('Zamiana w macierzy:'),
-	write('\n'),
-	write('[1, 2, 3][4, 5, 6][7, 8, 9]'),
-	write('\n'),
-	write('element [5] z [7]'),
-	write('\n'),
-	write(Result2),
-	write('\n'),
-	swap_advanced([[[1,11,111],[2,22,222],[3,33,333]],[[4,44,444],[5,55,555],[6,66,666]],[[7,77,777],[8,88,888],[9,99,999]]],[1,1],[3,3],Result444),
-	write('\n'),
-	write('Zamiana w macierzy:'),
-	write('\n'),
-	write('[[1,11,111],[2,22,222],[3,333,333]]'),
-	write('\n'),
-	write('[[4,44,444],[5,55,555],[6,66,666]]'),
-		write('\n'),
-	write('[[7,77,777],[8,88,888],[9,99,999]]'),
-	write('\n'),
-	write('element [1,11,111] z [9,99,999]'),
-	write('\n'),
-	write(Result444),
-	write('\n'),
-read(Opcja),
-	write('wybrano opcje = '), writeln(Opcja),
+	write('Macierz zosta³a dodana'),
 	write('\n').
+	
+podaj_id_macierzy(Id):-
+		write('\n'),
+		write('Podaj Id macierzy: '), read(Id).
+
+main_menu(MatrixCount):-
+	Max_Matrix = MatrixCount,
+	writeln('############################################################################'),
+	writeln('Baza danych'),
+	writeln('1 - wyswietl wszystkie macierze'),
+	writeln('2 - dodaj macierz do bazy danych'),
+	writeln('3 - Dodaj macierz X + Y'),
+	writeln('4 - Mno¿enie macierzy X przez skalar s '),
+	writeln('5 - Mno¿enie macierzy X przez macierz Y '),
+	writeln('6 - Transpozycja macierzy X'),
+	writeln('7 - Zamiana wierszy o indeksach i oraz j w macierzy X'),
+	writeln('8 - zamiana wierszy o indeksach [i1,i2] oraz [j1,j2] w macierzy X'),
+	writeln('9 - wyjscie z programu'),
+	read(Opcja),
+	write('wybrano opcje = '), writeln(Opcja),
+	( 
+		Opcja =:=1  -> menu_option(1, Max_Matrix), NewMatrixCount is Max_Matrix;
+		Opcja =:=2  -> menu_option(2, Max_Matrix), NewMatrixCount is Max_Matrix+1;
+		Opcja =:=3  -> menu_option(3), NewMatrixCount is Max_Matrix;
+		Opcja =:=4  -> menu_option(4), NewMatrixCount is Max_Matrix;
+		Opcja =:=5  -> menu_option(5), NewMatrixCount is Max_Matrix;
+		Opcja =:=6  -> menu_option(6), NewMatrixCount is Max_Matrix;
+		Opcja =:=7  -> menu_option(7), NewMatrixCount is Max_Matrix;
+		Opcja =:=8  -> menu_option(8), NewMatrixCount is Max_Matrix;
+		Opcja =:=9  -> menu_option(9);
+		write('Not implemented')
+	),
+	nl,
+main_menu(NewMatrixCount).
+
+menu_option(1,Max_Matrix):-
+	wyswietl_macierze(0,Max_Matrix).
+	
+menu_option(2, Max_Matrix):-
+dodaj_macierz(Max_Matrix).
+
+menu_option(3):-
+podaj_id_macierzy(Id),
+macierz_db(Id,X),
+write('X: '),
+write(X),
+write('\n'),
+podaj_id_macierzy(Id2),
+macierz_db(Id2,Y),
+write('Y: '),
+write(Y),
+write('\n'),
+write('\n'),
+write('Suma macierzy X+Y: '),
+add(X,Y,Wynik),
+write(Wynik),
+write('\n').
+
+menu_option(4):-
+podaj_id_macierzy(Id),
+macierz_db(Id,X),
+write('X: '),
+write(X),
+write('\n'),write('\n'),
+write('Podaj skalar s: '), read(S),
+write('s: '),
+write(S),
+write('\n'),
+write('\n'),
+write('Iloczyn X*s: '),
+scale(X, S, Wynik),
+write(Wynik),
+write('\n').
+
+menu_option(5):-
+podaj_id_macierzy(Id),
+macierz_db(Id,X),
+write('X: '),
+write(X),
+write('\n'),
+podaj_id_macierzy(Id2),
+macierz_db(Id2,Y),
+write('Y: '),
+write(Y),
+write('\n'),
+write('\n'),
+write('Iloczyn macierzy X*Y: '),
+multiply(X, Y, Wynik),
+write(Wynik),
+write('\n').
+
+menu_option(6):-
+podaj_id_macierzy(Id),
+macierz_db(Id,X),
+write('X: '),
+write(X),
+write('\n'),write('\n'),
+write('X po Transpozycji: '),
+transpose(X, Wynik),
+write(Wynik),
+write('\n').
+
+menu_option(7):-
+podaj_id_macierzy(Id),
+macierz_db(Id,X),
+write('X: '),
+write(X),
+write('\n'),write('\n'),
+write('Podaj indeks wiersza i: '),read(I),
+write('\n'),write('\n'),
+write('Podaj indeks wiersza j: '),read(J),
+write('\n'),write('\n'),
+write('Macierz X po zamianie wierszy i z j:'),
+swap(X,I,J,Wynik),
+write(Wynik),
+write('\n').
+
+menu_option(8):-
+podaj_id_macierzy(Id),
+macierz_db(Id,X),
+write('X: '),
+write(X),
+write('\n'),write('\n'),
+write('Podaj indeks (dwuelementowy) wiersza i: '),read(I),
+write('\n'),write('\n'),
+write('Podaj indeks (dwuelementowy) wiersza j: '),read(J),
+write('\n'),write('\n'),
+write('Macierz X po zamianie wierszy i z j:'),
+swap_advanced(X,I,J,Wynik),
+write(Wynik),
+write('\n').
+
+menu_option(9):-
+	writeln('nacisnij ENTER'),
+	halt,
+	!.
+	
 :- main.
